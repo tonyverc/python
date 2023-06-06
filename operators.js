93e200c856426cdfe7e80d82e426d524852b8633
tonyverc <tony.vercruysse@hotmail.com>
2023-06-06 16:11:20 +0200

    cours sur les operateurs

---
+++ operators.js
@@ -0,0 +1,11 @@
+console.log("hello js!");
+let number1 = 123;
+let text1 ="123";
+
+
+// comparaison d'egalité
+//renvoie true
+console.log(number1==text1);
+console.log()
+
+console.log(number1==text1);
---
+++ operators.py
@@ -0,0 +1,47 @@
+#arithmétique
+a= 123 + 42
+a= 123- 42
+a= 123 * 42
+a= 123 / 42
+
+#division entiére, integer division
+a=123 // 42
+print(a)
+
+#modulo ou reste de la division (euclidienne)
+a= 53
+#s'il y a un reste,le nombre n'est pas divisible par deux,donc il n'est pas pair
+print(a % 2)
+
+a = 74
+
+
+#puissance,exponentiation
+#deux puissance trois
+#equivalent de 2* 2 * 2
+a= 2 ** 3
+print(a)
+
+#opérateurs de comparaison
+#operateur d'egalité
+result = 123 == 42
+print(result)
+
+password ="abc"
+user_input = "def"
+print(password == user_input)
+
+#(strictement )plus grand que
+123 > 42  
+
+#plus grand ou égal à
+123 ou 42 >=42
+
+#plus petit que
+123 < 42
+
+#plus petit ou égal à
+123 <= 42
+
+#différent de
+123 != 42
